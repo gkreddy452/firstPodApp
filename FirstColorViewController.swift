c8482b049d6fb5aac98b3c21b3d47e4bddbc0e4b
gkreddy452 <kasturi.gopal@mac-14-15-142.local>
2017-09-22 15:41:05 +0530

    - Added new Class

---
+++ FirstColorViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstColorViewController.swift
+//  Pods
+//
+//  Created by kasturi gopal on 22/09/17.
+//
+//
+
+import Foundation
+import UIKit
+
+
+class FirstColorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.black
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
